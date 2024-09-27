997ba91bbf74ad70c1daf540a94584f4cd8f4ad4
KhoanDoanVan <doanvankhoan124@gmail.com>
2024-09-27 21:56:54 +0700

    first commit

--- CustomFrameworkProject/CustomFrameworkProjectApp.swift
+++ CustomFrameworkProject/CustomFrameworkProjectApp.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 @main
 struct CustomFrameworkProjectApp: App {
+    
+    init() {
+        SDK.doSomeWork()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ CustomFrameworkProject/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Đoàn Văn Khoan on 27/9/24.
+//
+
+import Foundation
+
+struct SDK {
+    static func doSomeWork() {
+        print("Doing some work ... ")
+    }
+}
